7ab5ac5f44504d7d1b315aa7aff5e95197b3cee1
FDosoriosarmiento <fabianrmdos@gmail.com>
2015-04-26 21:43:30 -0400

    Se configuro la vista para el admin

--- application/views/Administrador_View.php
+++ application/views/Administrador_View.php
@@ -1,34 +1,35 @@
 
-<h2> Utilidades</h2>
+<h2>Administrador SIVI-UQ</h2>
+<ul id="navegadorMenu" class="navegadorMenu">
+	<li>
+		<a id="formularioProyecto" class="formularioProyecto"  href="<?php echo base_url(); ?>index.php/Proyecto_Controller" value="Registrar Proyecto" type="submit" name="Registrar_Proyecto">
+		 	<span>Registrar Proyecto</span>
+	 	</a>
+	</li>
+	<li>
+		<a id="formularioInvestigador" class="formularioInvestigador" href="<?php echo base_url(); ?>index.php/Investigador_Controller" value="Registrar Investigador" type="submit" name="Registrar_Investigador">
+			<span>Registrar Investigador</span>
+		</a>
+	</li>
+	<li>
+		<a id="formularioListar" class="formularioListar" href="<?php echo base_url(); ?>index.php/Listar_Controller" value="Listar" type="submit" name="Listar">
+			<span>Listar</span>
+		</a>
+	</li>
+	<li>
+		<a id="formularioDConvocatoria" class="formularioDConvocatoria" href="<?php echo base_url(); ?>index.php/Administrador_Controller/descargarConvocatoria" value="Descargar Convocatoria" type="submit" name="Descargar_Convocatoria">
+			<span>Descargar Convocatoria</span>
+		</a>
+	</li>
+	<li>
+		<a id="formularioDFormato" class="formularioDFormato" href="<?php echo base_url(); ?>index.php/Administrador_Controller/descargarFormato" value="Descargar Formato" type="submit" name="Descargar_Formato">
+			<span>Descargar Formato</span>
+		</a>
+	</li>
+	<li>
+		<a id="formularioDCPresupuesto" class="formularioDCPresupuesto" href="<?php echo base_url(); ?>index.php/Administrador_Controller/descargarCuadro" value="Descargar Cuadro Presupuesto" type="submit" name="Descargar_Cuadro">
+			<span>Descargar Cuadro Presupuesto</span>
+		</a>
+	</li>
 
-<form id="formulario4" action="<?php echo base_url(); ?>index.php/Proyecto_Controller" name="form4">
- <input value="Registrar Proyecto" type="submit" name="Registrar_Proyecto">
- </form>
-<br>
-<form id="formulario3" action="<?php echo base_url(); ?>index.php/Investigador_Controller" name="form3">
- <input value="Registrar Investigador" type="submit" name="Registrar_Investigador">
-</form>
- <br>
-<br>
-
-<form id="formulario3" action="<?php echo base_url(); ?>index.php/Listar_Controller" name="form3">
- <input value="Listar" type="submit" name="Listar">
-</form>
- <br>
-<br>
-
-
-
-
-<h2>Archivos</h2>
-
- <br>
-<br>
-<form id="formulario3" action="<?php echo base_url(); ?>index.php/Administrador_Controller/descargarConvocatoria" name="form3">
- <input value="Descargar Convocatoria" type="submit" name="Descargar_Convocatoria">
-</form>
- <form  " formulario" action="<?php echo base_url(); ?>index.php/Administrador_Controller/descargarFormato" name="form">
- <input value="Descargar Formato" type="submit" name="Descargar_Formato">
-</form>
- <form id="formulario2" action="<?php echo base_url(); ?>index.php/Administrador_Controller/descargarCuadro" name="form2">
- <input value="Descargar Cuadro Presupuesto" type="submit" name="Descargar_Cuadro">    </form> 
+</ul>
