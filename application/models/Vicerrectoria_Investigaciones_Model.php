09b19e8100a6a4b10ec68171064780dc2a3f34e5
sebas8632 <sebastian8632@gmail.com>
2015-05-19 01:57:05 -0500

    se hace el reporte.
    se hace el reporte si se aprobo o no el proyecto por vicerrectoria.

--- application/models/Vicerrectoria_Investigaciones_Model.php
+++ application/models/Vicerrectoria_Investigaciones_Model.php
@@ -37,20 +37,70 @@ class Vicerrectoria_Investigaciones_Model extends CI_Model{
 		foreach ($query->result() as $row){
 			$cumple= $row->ESTADO;
 		}
-		if($cumple!='Aprobado'){
+		if($cumple!='Aprobado' && $cumple!="No Aprobado"){
 			if($decision=='Aprobado'){
 					
 				$data= array('ESTADO'=>$decision);
 				$this->db->where('FK_PROYECTO', $id);
 				$this->db-> update('VICERRECTORIA_INVESTIGACION', $data);
+				$this->generarArchivo($id,$decision);
 			}else{
 				if($decision== "No Aprobado"){
 					$data= array('ESTADO'=> $decision);
 					$this->db->where('FK_PROYECTO', $id);
 					$this->db-> update('VICERRECTORIA_INVESTIGACION', $data);
+					$this->generarArchivo($id,$decision);
 				}
 			}
 		}
+		
+	}
+	
+	function generarArchivo($id, $decision){
+		
+		//se consulta el titulo del proyecto
+		$this->db->where('NUMERO',$id);
+		$nombre_Proy=$this->db->get('PROYECTO');
+		foreach ($nombre_Proy->result() as $row){
+			$nombre_Proyecto= $row->TITULO;
+		}
+		//se consulta el id y el nombre del investigador
+		$this->db->where('FK_PROYECTO', $id);
+		$idInvest=$this->db->get('PROYECTO_INVESTIGADOR');
+		foreach ($idInvest->result() as $row){
+			$id_Investigador= $row->FK_INVESTIGADOR;
+		}
+		$this->db->where('DOCUMENTO', $id_Investigador);
+		$nombreInves=$this->db->get('INVESTIGADOR');
+		foreach ($nombreInves->result() as $row){
+			$nombre_Investigador= $row->NOMBRE;
+		}
+		
+		$f1= fopen("respuesta_vicerrectoria.txt", "a") or die ("problemas al crear archivo");
+		
+		if($decision=="Aprobado"){
+		
+		fwrite($f1, "Vicerrectoria de Investigaciones");
+		fwrite($f1,"\n"."\n");
+		fwrite($f1,"Estimado ".$nombre_Investigador." con identificación: ".$id_Investigador."\n");
+		fwrite($f1,"  le informamos que el proyecto ".$nombre_Proyecto." tuvo una calificación satisfactoria, por favor acercarse a esta dependencia para dar inicio al proyecto.");
+		fwrite($f1, "\n"."\n"."\n". "PATRICIA LANDAZURI MSc PHD.");
+		
+		fclose($f1);
+		}
+		else{
+			if($decision=="No Aprobado"){
+				fwrite($f1, "Vicerrectoria de Investigaciones");
+				fwrite($f1,"\n"."\n");
+				fwrite($f1,"Estimado ".$nombre_Investigador." con identificación: ".$id."\n");
+				fwrite($f1,"  le informamos que el proyecto ".$nombre_Proyecto." no tuvo una calificación satisfactoria, puede acogerse al art.37 del Estatuto de investigaciones.");
+				fwrite($f1, "\n"."\n"."\n". "PATRICIA LANDAZURI MSc PHD.");
+				fclose($f1);
+			}
+		}
+		
+		
+		
 	}
 
 }
